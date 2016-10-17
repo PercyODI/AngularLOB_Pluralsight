a7f933de2eda7352997cddbe120f4d47ae3c0317
PercyODI <pearse.2008@gmail.com>
2016-10-16 21:11:51 -0500

    Working on Routing

--- common/services/productResourceMock.js
+++ common/services/productResourceMock.js
@@ -93,6 +93,24 @@
             return [200, product, {}];
         });
 
-        $httpBackend.whenPOST(productUrl)
+        $httpBackend.whenPOST(productUrl).respond(function (method, url, data) {
+            var product = angular.fromJson(data);
+
+            if (!product.productId) {
+                product.productId = products[products.length - 1].productId + 1
+            }
+            else {
+                for (var i = 0; i < products.length; i++) {
+                    if (products[i].productId == product.productId) {
+                        products[i] = product;
+                        break;
+                    }
+                }
+            }
+
+            return [200, product, {}];
+        });
+
+        $httpBackend.whenGET(/app/).passThrough();
     });
 }());
